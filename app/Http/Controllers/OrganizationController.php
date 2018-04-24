c9a212f343571be6e542d6b954c5830e933ad7cf
Gerald Derrick <jerry.derrick@gmail.com>
2018-04-24 00:02:55 -0500

    Added models for organizations and User_infos

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Organization;
 use Illuminate\Http\Request;
 
 class OrganizationController extends Controller
 {
-    //
+    public function list($owner_user_id)
+    {
+        $organizations=Organization::orderBy('owner_user_id');
+        dump($owner_user_id);
+        dump($organizations);
+//        $books = Book::orderBy('title')->get();
+//        $newBooks = $books->sortByDesc('created_at')->take(3);
+//        $alert = $request->session()->get('alert');
+//
+//        return view('books.index')->with([
+//            'books' => $books,
+//            'newBooks' => $newBooks,
+//            'alert' => $alert
+//        ]);
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,11 @@ Route::get('oauth/{driver}', 'Auth\SocialAuthController@redirectToProvider')->na
 Route::get('oauth/{driver}/callback', 'Auth\SocialAuthController@handleProviderCallback')->name('social.callback');
 
 // Organization Routes
-Route::get('/org/{user_id}/list')->name('org.list')->middleware('auth'); // List all organizations owned/managed by a user
-Route::get('/org/show/{organization_id}')->name('org.show')->middleware('auth'); // View an organization details (if authorized)
-Route::get('/org/{user_id}/new')->name('org.new')->middleware('auth'); // Add a new organization
-Route::get('/org/{user_id}/edit/{organization_id}')->name('org.edit')->middleware('auth'); // Edit an organization
-Route::get('/org/{user_id}/del/{organization_id}')->name('org.edit')->middleware('auth'); // Delete an organization
+Route::get('/org/{owner_user_id}/list','OrganizationController@list')->name('org.list')->middleware('auth'); // List all organizations owned/managed by a user
+Route::get('/org/show/{id}')->name('org.show')->middleware('auth'); // View an organization details (if authorized)
+Route::get('/org/{owner_user_id}/new')->name('org.new')->middleware('auth'); // Add a new organization
+Route::get('/org/{owner_user_id}/edit/{id}')->name('org.edit')->middleware('auth'); // Edit an organization
+Route::get('/org/{owner_user_id}/del/{id}')->name('org.edit')->middleware('auth'); // Delete an organization
 
 // User Info Routes
+Route::get('/userinfo/show/{id}')->name('org.show')->middleware('auth');
